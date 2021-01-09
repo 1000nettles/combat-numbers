4cb93b6b3d9bb3ecab5d9f844cfccd4f123054c6
1000nettles <1000nettles@users.noreply.github.com>
2021-01-09 10:49:27 -0800

    Add color, bold, italic and other font choices
    * Previously, we had very limited selections in the appearance config
      for Combat Numbers. We've expended these options.
    * We still have not wired these up to the AmountStylizer.

--- src/module/combatNumbersConfig.js
+++ src/module/combatNumbersConfig.js
@@ -1,11 +1,19 @@
 /* global FormApplication */
 /* global game */
+/* global jQuery */
 /* global mergeObject */
 
 /**
  * Form application to configure settings of Combat Numbers.
+ *
+ * Currently, specific to appearance settings.
  */
 export default class CombatNumbersConfig extends FormApplication {
+  constructor(object = {}, options = {}) {
+    super(object, options);
+    this.fontOther = game.i18n.localize('COMBATNUMBERS.SETTINGS.fontOther');
+  }
+
   /** @override */
   static get defaultOptions() {
     return mergeObject(super.defaultOptions, {
@@ -13,7 +21,7 @@ export default class CombatNumbersConfig extends FormApplication {
       id: 'combat-numbers-config',
       template: 'modules/combat-numbers/src/templates/config.html',
       width: 500,
-      height: 150,
+      height: 310,
       closeOnSubmit: true,
     });
   }
@@ -26,6 +34,10 @@ export default class CombatNumbersConfig extends FormApplication {
   static get DEFAULT_APPEARANCE() {
     return {
       font: 'Verdana',
+      damageColor: '#ffffff',
+      healColor: '#95ed98',
+      bold: true,
+      italic: false,
     };
   }
 
@@ -53,10 +65,18 @@ export default class CombatNumbersConfig extends FormApplication {
 
   /** @override */
   getData() {
-    return {
-      fontList: CombatNumbersConfig.FONT_FAMILIES,
-      selectedFont: game.settings.get('combat-numbers', 'font'),
+    const appearance = game.settings.get('combat-numbers', 'appearance');
+    const defaultAppearance = CombatNumbersConfig.DEFAULT_APPEARANCE;
+    const object = {
+      fontList: this._getFontList(),
+      font: appearance.font ?? defaultAppearance.font,
+      damageColor: appearance.damageColor ?? defaultAppearance.damageColor,
+      healColor: appearance.healColor ?? defaultAppearance.healColor,
+      bold: appearance.bold ?? defaultAppearance.bold,
+      italic: appearance.italic ?? defaultAppearance.italic,
     };
+
+    return object;
   }
 
   /** @override */
@@ -67,8 +87,34 @@ export default class CombatNumbersConfig extends FormApplication {
     const appearance = game.settings.get('combat-numbers', 'appearance');
     const fontKey = this._getFontKeyByName(appearance.font);
 
+    // As we cannot set the current selected `option` via Handlebars, we have
+    // to do it with jQuery instead.
     html.find('select[name="font"]').val(fontKey);
 
+    const fontOtherFormGroup = html.find('.form-group-font-other');
+    const fontOther = html.find('#fontOther');
+
+    // If the user has a custom defined font, fill out the "font other" field.
+    if (this._getFontList()[fontKey] === this.fontOther) {
+      fontOtherFormGroup.show();
+      fontOther.val(appearance.font);
+    }
+
+    // Show / hide the "font other" box only if the "Other" option is selected.
+    const fontOtherName = this.fontOther;
+    html.find('select[name="font"]').change(function () {
+      const optionName = jQuery(this).find('option:selected').text();
+
+      if (optionName !== fontOtherName) {
+        fontOther.val('');
+        fontOtherFormGroup.hide();
+        return;
+      }
+
+      fontOtherFormGroup.show();
+    });
+
+    // Perform the reset action if the user clicks "Reset".
     html.find('button[name="reset"]').click(() => {
       this.reset(html);
     });
@@ -78,8 +124,14 @@ export default class CombatNumbersConfig extends FormApplication {
   async _updateObject(event, formData) {
     const appearance = {};
 
-    const selectedFont = parseInt(formData.font, 10);
-    appearance.font = CombatNumbersConfig.FONT_FAMILIES[selectedFont];
+    appearance.font = this._getSelectedFont(
+      parseInt(formData.font, 10),
+      formData,
+    );
+    appearance.damageColor = formData.damageColor;
+    appearance.healColor = formData.healColor;
+    appearance.bold = formData.bold;
+    appearance.italic = formData.italic;
 
     await game.settings.set(
       'combat-numbers',
@@ -99,6 +151,12 @@ export default class CombatNumbersConfig extends FormApplication {
     const fontKey = this._getFontKeyByName(defaultAppearance.font);
 
     html.find('select[name="font"]').val(fontKey);
+    html.find('input[name="damageColor"]').val(defaultAppearance.damageColor);
+    html.find('input[name="damageColorSelector"]').val(defaultAppearance.damageColor);
+    html.find('input[name="healColor"]').val(defaultAppearance.healColor);
+    html.find('input[name="healColorSelector"]').val(defaultAppearance.healColor);
+    html.find('input[name="bold"]').prop('checked', defaultAppearance.bold);
+    html.find('input[name="italic"]').prop('checked', defaultAppearance.italic);
   }
 
   /**
@@ -114,8 +172,65 @@ export default class CombatNumbersConfig extends FormApplication {
    * @private
    */
   _getFontKeyByName(name) {
-    return CombatNumbersConfig.FONT_FAMILIES.findIndex(
+    const fontList = this._getFontList();
+    const foundFontKey = fontList.findIndex(
       (font) => font === name,
     );
+
+    // If we couldn't find the font, find the "Other" option and return that
+    // key.
+    if (foundFontKey === -1) {
+      return fontList.findIndex(
+        (font) => font === this.fontOther,
+      );
+    }
+
+    return foundFontKey;
+  }
+
+  /**
+   * Get the font list to display in the configuration dialog.
+   *
+   * @return {Array}
+   *
+   * @private
+   */
+  _getFontList() {
+    const fonts = CombatNumbersConfig.FONT_FAMILIES;
+    fonts.push(this.fontOther);
+
+    return fonts;
+  }
+
+  /**
+   * Get the selected font by the selected key from the font list.
+   *
+   * If the user has chosen "other", we will find the font they typed in and
+   * use that instead.
+   *
+   * @param {number} selectedFontKey
+   *   The selected key of the font list.
+   * @param formData {Object}
+   *   The object of validated form data with which to update the object.
+   *
+   * @return {string}
+   *
+   * @private
+   */
+  _getSelectedFont(selectedFontKey, formData) {
+    const fontList = this._getFontList();
+    const selected = fontList[selectedFontKey];
+
+    if (selected === this.fontOther) {
+      // If for some reason the user hasn't typed in a custom font, just
+      // use the default.
+      if (!formData?.fontOther) {
+        return CombatNumbersConfig.DEFAULT_APPEARANCE.font;
+      }
+
+      return String(formData.fontOther).trim();
+    }
+
+    return selected;
   }
 }
